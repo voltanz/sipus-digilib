ba6c92d9d6a13a1ab9d823827ac0e6fce6d6fe30
askholani <askholani210@gmail.com>
2023-05-24 09:44:37 +0700

    memperbaiki link

--- resources/views/admin/template/default.blade.php
+++ resources/views/admin/template/default.blade.php
@@ -13,7 +13,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{asset('assets/logo/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ asset('assets/logo/logo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">DIGILIB</span>
             </a>
 
@@ -34,7 +35,6 @@
 
     </div>
     <!-- ./wrapper -->
-
     @include('admin/template/partials/scripts')
 </body>
 
--- resources/views/frontend/templates/partials/nav.blade.php
+++ resources/views/frontend/templates/partials/nav.blade.php
@@ -7,50 +7,6 @@ if ($user = auth()->user()) {
 }
 ?>
 
-{{-- <div class="navbar-fixed">
-    <nav class="teal lighten-2">
-        <div class="container">
-            <div class="nav-wrapper">
-                <a href="#!" class="brand-logo"><img src="{{ asset('assets/logo/sipus.png') }}"
-                        style="width: 60px;"></a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="{{ route('homepage') }}">Koleksi Buku</a></li>
-                    <li><a href="badges.html">Layanan</a></li>
-                    <li><a href="collapsible.html">Syarat & Ketentuan</a></li>
-                    @guest
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        @if (Route::has('register'))
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @endif
-                    @else
-                        <li>
-                            <a href="{{ route('pinjam.index') }}"><i class="material-icons">shopping_basket</i> (
-                                {{ $count }} )</a>
-                        </li>
-                        <ul id="dropdown2" class="dropdown-content">
-                            <li><a href="{{ route('home.profile') }}">Profil</a></li>
-                            <li><a href="{{ route('transaksi.index') }}">Peminjaman</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#!"
-                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    Logout</a></li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </ul>
-                        <li><a class="dropdown-trigger" href="#" data-target="dropdown2">
-                                {{ ucwords(Auth()->user()->name) }}<i class="material-icons right">arrow_drop_down</i></a>
-                        </li>
-                    @endguest
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-</div> --}}
-
 <nav class="navbar navbar-expand-lg bg-dark d-flex flex-column w-full">
     <ul class="d-flex justify-content-start w-100  border-1 border-bottom border-white h-25" style="font-size: 12px">
         <li class="pe-4">
