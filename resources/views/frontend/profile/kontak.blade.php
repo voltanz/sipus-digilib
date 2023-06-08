15be7a8dfdcfc0e4773eec12bd84567ad2701ef8
nikoshaa <hfdzywlldn@gmail.com>
2023-06-08 18:06:33 +0700

    Fix kontak

--- resources/views/frontend/profile/kontak.blade.php
+++ resources/views/frontend/profile/kontak.blade.php
@@ -13,19 +13,19 @@
             <div class="col-6">
                 <div class="me-2">
                 <h3><b>DIGILIB SIPUS</b></h3><br>
-                <p class="text-left" style="font-size: 18px;">Digital Library of SMA Negeri 1 Blega</p>
-                <h4><b></b></h4>
-                <p class="text-left" style="font-size: 18px;">Kedong, Rosep, Kec. Blega, Kabupaten Bangkalan, Jawa Timur, 69174, ID</p>
-                <h4><b></b></h4>
-                <p class="text-left" style="font-size: 18px;">E: perpus@sman1blega.ac.id</p>
-                <p class="text-left" style="font-size: 18px;">P: (021) 56958721 ext. 1000</p>
-                <p class="text-left" style="font-size: 18px;">P: (021) 56958721</p>
+                <p class="text-left" style="font-size: 15px;">Digital Library of SMA Negeri 1 Blega</p>
+                <hr />
+                <p class="text-left" style="font-size: 15px;">Kedong, Rosep, Kec. Blega, Kabupaten Bangkalan, Jawa Timur, 69174, ID</p>
+                <hr />
+                <p class="text-left" style="font-size: 15px;">E: perpus@sman1blega.ac.id</p>
+                <p class="text-left" style="font-size: 15px;">P: (021) 56958721 ext. 1000</p>
+                <p class="text-left" style="font-size: 15px;">P: (021) 56958721</p>
                 <h4><b></b></h4>
                 <br><br>
               </div>
             </div>
             <div class="col-6">
-            <img src="{{ asset('assets/logo/perpus.png') }}" style="width: 800px;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1664.5425972403946!2d113.0490653522485!3d-7.129258549470115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd82102e9471ff5%3A0xb654904772599bb6!2sSMA%20Negeri%201%20Blega!5e0!3m2!1sid!2sid!4v1686222265043!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <br><br><br>
             </div>
         </div>
