db8d6df7cc91a247f9bdb7b17683a852325ed168
Iemaduddin <iemaduddin17@gmail.com>
2023-06-13 17:33:41 +0700

    Update Link Sipus Smansaga ke Home di Dashboard

--- resources/views/admin/template/default.blade.php
+++ resources/views/admin/template/default.blade.php
@@ -12,7 +12,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="/" class="brand-link">
 
                 <img src="{{ asset('assets/logo/smansaga.png') }}" alt="smansaga.png" class="brand-image"
                     style="opacity: .8">
