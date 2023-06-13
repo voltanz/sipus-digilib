6a912c3c5ec0b9020856b9c5c215d3cdd57e5923
voltanz <bimatris13@gmail.com>
2023-06-13 14:42:27 +0700

    Merapihkan layout profile

--- resources/views/frontend/profile/kontak.blade.php
+++ resources/views/frontend/profile/kontak.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="containe-fluid py-5 d-flex flex-column align-items-center">
-        <h2 class="text-center text-capitalize"><b>Kontak Perpustakaan</b></h2>
+        <h2 class="text-center text-capitalize">::Kontak Perpustakaan::</h2>
     </div>
     <!-- <center>
     <img src="{{ asset('assets/logo/perpus.png') }}" style="width: 900px;"></a>
@@ -22,7 +22,7 @@
                 <p class="text-left" style="font-size: 15px;">P: (021) 56958721</p><br><br>
               </div>
             </div>
-            <div class="col-6">
+            <div class="col-6" style="padding-bottom: 7rem">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1664.5425972403946!2d113.0490653522485!3d-7.129258549470115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd82102e9471ff5%3A0xb654904772599bb6!2sSMA%20Negeri%201%20Blega!5e0!3m2!1sid!2sid!4v1686222265043!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <br><br><br>
             </div>
