fff638c03870d5a55f55a924617dc2eee8fd1744
Iemaduddin <iemaduddin17@gmail.com>
2023-06-16 07:16:17 +0700

    Bagian user peminjaman

--- resources/views/admin/template/default.blade.php
+++ resources/views/admin/template/default.blade.php
@@ -8,7 +8,7 @@
     <div class="wrapper">
         @include('admin/template/partials/nav')
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="index3.html" class="brand-link">
+            <a href="/" class="brand-link">
 
                 <img src="{{ asset('assets/logo/smansaga.png') }}" alt="smansaga.png" class="brand-image"
                     style="opacity: .8">
@@ -28,4 +28,5 @@
     </div>
     @include('admin/template/partials/scripts')
 </body>
+
 </html>
--- resources/views/admin/template/partials/head.blade.php
+++ resources/views/admin/template/partials/head.blade.php
@@ -3,10 +3,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('assets/logo/smansaga.png') }}" />
     <title>@yield('title') | Sipus SMAN 1 Blega</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    {{-- <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet"
         href="{{ asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
